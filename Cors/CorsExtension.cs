b60e518748f076221fa4d408d78eb3367f7c9b03
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-11-18 12:52:14 +0330

    Add project files.

---
+++ Cors/CorsExtension.cs
@@ -0,0 +1,121 @@
+﻿//نحوه استفاده در زیر کد
+// نیاز به این پکیج است Microsoft.Aspnet.Core.Cors
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Api.Dependency.Cors
+{
+    /// <summary>
+    ///  CorsExtension 
+    /// </summary>
+    public static class CorsExtension
+    {
+        /// <summary>
+        /// // های مورد نیاز Cors اضافه کردن 
+        /// </summary>
+        /// <param name="services">IServiceCollection</param>
+        /// <param name="corsOrigin"> string[] corsOrigin</param>
+        /// <param name="corsMethod">string[] corsMethod</param>
+        /// <example>
+        /// in AppSetting.Json file
+        /// <code>
+        /// "Cors": {
+        /// "Origin": [ "Https://localhost:3000", "Https://localhost:3001" ],
+        /// "Method": [ "OPTIONS","GET","HEAD","POST","PUT","DELETE" ]
+        /// },
+        /// </code>
+        /// i serviceConfig StartUp.cs file
+        /// var corsOrigin = Configuration.GetSection("Cors:Origin").Get&lt;string[]&gt;();
+        /// var corsMethod = Configuration.GetSection("Cors:Method").Get&lt;string[]&gt;();
+        /// services.AddOurCors(corsOrigin, corsMethod);
+        /// </example>
+        /// <returns> service </returns>
+
+        public static IServiceCollection AddOurCors(this IServiceCollection services,
+            string[] corsOrigin, string[] corsMethod)
+        {
+            if (corsOrigin.Length == 0 && corsMethod.Length == 0)
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(
+                        builder =>
+                        {
+                            builder
+                                .AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .AllowCredentials();
+                        });
+                });
+                return services;
+
+            }
+            if (corsOrigin.Length > 0 && corsMethod.Length == 0)
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(
+                        builder =>
+                        {
+                            builder
+                                .WithOrigins(corsOrigin)
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .AllowCredentials();
+                        });
+                });
+                return services;
+
+            }
+            if (corsOrigin.Length > 0 && corsMethod.Length > 0)
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(
+                        builder =>
+                        {
+                            builder
+                                .WithOrigins(corsOrigin)
+                                .WithMethods(corsMethod)
+                                .AllowAnyHeader()
+                                .AllowCredentials();
+                        });
+                });
+                return services;
+
+            }
+            if (corsOrigin.Length == 0 && corsMethod.Length > 0)
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(
+                        builder =>
+                        {
+                            builder
+                                .AllowAnyOrigin()
+                                .WithMethods(corsMethod)
+                                .AllowAnyHeader();
+                        });
+                });
+                return services;
+
+            }
+            return services;
+        }
+    }
+}
+//نحوه استفاده
+
+//#region AppSettingCors
+////"Cors": {
+////"Origin": [ "Https://localhost:3000", "Https://localhost:3001" ],
+////"Method": [ "OPTIONS","GET","HEAD","POST","PUT","DELETE" ]
+////},
+//#endregion
+
+//#region ServiceConfig
+////var corsOrigin = Configuration.GetSection("Cors:Origin").Get<string[]>();
+////var corsMethod = Configuration.GetSection("Cors:Method").Get<string[]>();
+////services.AddOurCors(corsOrigin, corsMethod);
+//#endregion
+
---
+++ Swagger/SwaggerExtension.cs
@@ -0,0 +1,82 @@
+﻿//نحوه استفاده در ذیل کد
+// پکیج مورد استفاده  <PackageReference Include="Swashbuckle.AspNetCore" Version="5.6.3" />
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+
+namespace Common.Api.Dependency.Swagger
+{
+    /// <summary>
+    /// SwaggerExtension with Authorize
+    /// </summary>
+    public static class SwaggerExtension
+    {
+        /// <summary>
+        /// Swagger تنظیمات
+        /// </summary>
+        /// <remarks>
+        /// استفاده می شود Extension از این Authorize با کلید Swagger جهت استفاده از
+        /// </remarks>
+        /// <example>
+        /// نحوه استفاده
+        /// <code>
+        /// services.AddOurSwagger();
+        /// </code>
+        /// </example>
+        /// <param name="services">services</param>
+        /// <returns>services</returns>
+        public static IServiceCollection AddOurSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = " API",
+                    Description = "just for test api",
+
+
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under Boursyar",
+
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+
+            });
+            return services;
+        }
+    }
+}
+
+//#region How use it
+
+////services.AddOurSwagger();
+
+
+//#endregion
